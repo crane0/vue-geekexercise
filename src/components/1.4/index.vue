4ba47a34c85ab16be2342d6f48e275c8c5fa4617
crane0 <bhad5683126@163.com>
2019-10-18 01:22:00 +0800

    10,11节

---
+++ src/components/1.4/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p>
+      <button @click="handleNameChange">change this.name</button>
+      <button @click="handleInfoChange">change this.info</button>
+      <button @click="handleListChange">change this.list</button>
+    </p>
+    <PropsAndData :name="name" :info="info" :list="list" />
+  </div>
+</template>
+<script>
+import PropsAndData from "./PropsAndData";
+let name = "world";
+export default {
+  components: {
+    PropsAndData
+  },
+  data() {
+    this.name = name; 
+    return {
+      info: {},
+      // info: {
+      //   number: undefined
+      // },
+      list: []
+    };
+  },
+  methods: {
+    handleNameChange() {
+       // 不会触发更新，是因为没有做响应式
+      this.name = "vue" + Date.now();
+      console.log("this.name 发生了变化，但是并没有触发子组件更新", this.name);
+    },
+    handleInfoChange() {
+      // 相应式只存在 info 上，进一步的字段是没有响应式的。
+      // 只有在初始化的时候，就提前声明number，data在实例化时，才会进行响应式的设置。
+      this.info.number = 1;
+      // this.$set(this.info, 'number', 1)
+      console.log("this.info 发生了变化，但是并没有触发子组件更新", this.info);
+    },
+    handleListChange() {
+      this.list.push(1, 2, 3);
+      console.log("this.list 并没有发生变化，但是触发了子组件更新", this.list);
+    }
+  }
+};
+</script>
